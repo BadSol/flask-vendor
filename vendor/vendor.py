77e9df2566266bd6231ece1ff8c3f3619ba64109
Sol <rygula.dawid@gmail.com>
2016-08-11 17:17:08 +0200

    Added sqlalchemy support, added SECRETS.json file for sensitive data

--- vendor/vendor.py
+++ vendor/vendor.py
@@ -1,5 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import json
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# secrets = None
+with open('SECRETS.json') as data_file:
+    secrets = json.load(data_file)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@localhost/{}'.format(
+    secrets["db_user"],
+    secrets["db_password"],
+    secrets["db_name"])
+
+Base = SQLAlchemy(app)
 
 
 @app.route("/")
