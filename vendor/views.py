efba55fdcb0b43fdcfe2e28795a144c08995ab78
Sol <rygula.dawid@gmail.com>
2016-08-22 19:00:45 +0200

    refactoring

--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from vendor.models import User
+from vendor.models import User, db
 
 
 vendor_views = Blueprint('vendor', __name__, url_prefix='/')
@@ -7,5 +7,6 @@ vendor_views = Blueprint('vendor', __name__, url_prefix='/')
 
 @vendor_views.route('/')
 def index():
-    context = User.query().count()
+    # context = db.session.query(User).all()  # both doesn't work TypeError: 'User' object is not callable
+    context = User.query().all()  # TypeError: 'BaseQuery' object is not callable
     return context
