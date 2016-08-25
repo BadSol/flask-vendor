90317b8c21826ec1bfac03da35c65c37bb07416d
Sol <rygula.dawid@gmail.com>
2016-08-25 15:20:16 +0200

    Making queries works now. Added index view

--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from vendor.models import User, db
 
 
@@ -8,5 +8,6 @@ vendor_views = Blueprint('vendor', __name__, url_prefix='/')
 @vendor_views.route('/')
 def index():
     # context = db.session.query(User).all()  # both doesn't work TypeError: 'User' object is not callable
-    context = User.query().all()  # TypeError: 'BaseQuery' object is not callable
-    return context
+    # user = User.query.all()  # TypeError: 'BaseQuery' object is not callable
+    user = db.session.query(User).all()
+    return render_template('index.html', user=user[0].email)
