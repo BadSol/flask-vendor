a492d801ddc9f6ca385775d839af78d935b2bbce
Sol <rygula.dawid@gmail.com>
2016-08-27 21:00:48 +0200

    Basic templates using bootstrap are working. Started working on login view

--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from vendor.models import User, db
+from flask import Blueprint, render_template, request, redirect, url_for
+from vendor.models import User, db, bcrypt
 from flask_login import LoginManager
 
 login_manager = LoginManager()
@@ -13,10 +13,27 @@ def index():
     # user = User.query.all()  # TypeError: 'BaseQuery' object is not callable
     user = db.session.query(User).first()
 
-    return render_template('base.html', user=user)
+    return render_template('index.html', user=user.email)
 
 
-@vendor_views.route('login')
+@vendor_views.route('login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
 
+        user = load_user(request.form['email'])
+        if validate_login(user, request.form['password']):
+            print 'User {} logged in!'.format(user.name)
+        else:
+            redirect(url_for('vendor.login'))
+
+    else:
+        return render_template('login.html')
+
+
+@login_manager.user_loader
+def load_user(user_email):
+    return User.query.filter(User.email == user_email).one_or_none()
+
+
+def validate_login(user, password):
+    return bcrypt.check_password_hash(user.password, password)
